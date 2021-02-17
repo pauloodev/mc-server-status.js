5f8aed56b9a31d39020ec21b89d9fbf2fa8985d3
Pauloo <48892662+pauloodev@users.noreply.github.com>
2021-02-17 20:07:16 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,22 +1,21 @@
-const Discord = require('discord.js');
-const fetch = require('node-fetch');
-const client = new Discord.Client();
-client.login('YOUR_TOKEN_HERE');
+const Discord = require('discord.js'); // Important to install "npm i discord.js"
+const fetch = require('node-fetch'); // Important to install "npm i node-fetch"
+const client = new Discord.Client(); // Bring the Discord.Client function to a variable "client"
+client.login('YOUR_TOKEN_HERE'); // Your bot's Token HERE!
 
-function status() {
+function status() { // Minecraft Server Status System
     const mcIP = 'YOUR_SERVER_IP'; // Ex: hypixel.net
     var mcPort = YOUR_SERVER_PORT; // Ex: 25565
-    var url = 'http://mcapi.us/server/status?ip=' + mcIP + '&port=' + mcPort;
+    var url = 'http://mcapi.us/server/status?ip=' + mcIP + '&port=' + mcPort; // API (Here he will search the api)
     fetch(url)
         .then(res => res.json())
         .then(json => {
-
-            let players_status = client.channels.cache.get('ID_CHAT_VOICE');
-            players_status.setName(`🎮 Players: ${json.players.now}`)
+            let players_status = client.channels.cache.get('ID_CHAT_VOICE'); // IMPORTANT: Place the Voice Chat ID here where the member counter will be located
+            players_status.setName(`🎮 Players: ${json.players.now}`) // IMPORTANT: You can modify the message, but leave the variable "${json.players.now}" (is where the value sought in the api will appear)
         })
 }
 
-client.on("ready", () => {
-    console.log(`[BOT] - Bot is connected in ${client.user.tag}.`)
+client.on("ready", () => { // Bot Ready
+    console.log(`[BOT] - Bot is connected in ${client.user.tag}.`) // When the bot connects it will return this message on the console
     setInterval(status, 120000); // Time to update! Ex: 60000 = 1 minute
 })
